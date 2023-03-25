935b7328cace2aaad88aaca69e3a3a8c63228d23
Son Nguyen <95949597+sonnguyen1510@users.noreply.github.com>
2023-03-25 22:58:17 +0700

    v1.01
    -update index.pug with richtext
    -update socket.io

--- app.js
+++ app.js
@@ -1,54 +1,66 @@
-const createError = require('http-errors')
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-const socket = require('socket.io')
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const socket = require("socket.io");
 
-const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
-const { resourceUsage } = require('process')
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const { resourceUsage } = require("process");
 
-const app = express()
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
-})
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
-const server = app.listen('3000', () => {
-    console.log(`Server started on http://localhost:3000`)
-})
+const server = app.listen("3000", () => {
+  console.log(`Server started on http://localhost:3000`);
+});
 
-const io = socket(server)
-io.on('connection', (socket) => {
-    console.log(`Socket connected: ${socket.id}`)
+const io = socket(server);
 
-    socket.on('disconnect', () => {
-        console.log(`Socket disconnected: ${socket.id}`)
-    })
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("join", (roomId) => {
+    socket.join(roomId);
+    console.log("user joined at room " + roomId);
+  });
+
+  socket.on("textChange", (dataRC) => {
+    const { inputVal, RoomId } = dataRC;
+    console.log("text From room " + RoomId + ":" + inputVal);
+    socket.to(RoomId).emit("UpdateText", inputVal);
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/:RoomName", function (req, res, next) {
+  res.render("index", { RoomName: req.params.RoomName });
+});
+
+router.get("/test/", function (req, res, next) {
+  res.render("test");
 });
 
 module.exports = router;
